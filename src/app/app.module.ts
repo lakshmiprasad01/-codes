3dbba2bb222c87da2e009709834eacc8f1abd74d
lakshmiprasad <prasad.malakala@xyramsoft.com>
2022-03-03 21:09:14 +0530

    added a new file

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
